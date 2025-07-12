6f64e1f28d8bc5fa9b75114fe98784ef79877e4b
BonggusCha <aayy4321@naver.com>
2025-07-12 12:19:15 +0900

    처음 설정 커밋

---
+++ src/test/java/com/smuclub/smu_club/SmuClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smuclub.smu_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmuClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
