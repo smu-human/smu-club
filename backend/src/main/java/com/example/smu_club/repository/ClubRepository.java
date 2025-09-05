74a4b9d561889e746618555f85e66a667d3313a0
FluanceIFI <gksrnr66@gmail.com>
2025-09-05 18:28:50 +0900

    GET /api/v1/public/clubs 의 JSON 스펙 변경
    ClubResponseDto 필드에서 members(동아리 회원 수) 필드 삭제

---
+++ backend/src/main/java/com/example/smu_club/repository/ClubRepository.java
@@ -0,0 +1,29 @@
+package com.example.smu_club.repository;
+
+import com.example.smu_club.controller.GuestController;
+import com.example.smu_club.domain.Club;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ClubRepository {
+
+    @PersistenceContext //Spring Entity Manager를 가져올 수 있게 해줌.
+    private final EntityManager em;
+
+    public Club findOne(Long id){
+        return em.find(Club.class, id);
+    }
+
+    public List<Club> findAll(){
+        return em.createQuery("select c from Club c", Club.class).getResultList();
+    }
+
+
+}
