8e4af0a4fce98f6f188a7905c1ea152b1f2cbefa
BonggusCha <aayy4321@naver.com>
2025-09-22 13:14:32 +0900

    Refactor: 레포지토리 중복 처리 및 복합키 제거 관련 수정

--- backend/src/main/java/com/example/smu_club/club/repository/ClubRepository.java
+++ backend/src/main/java/com/example/smu_club/club/repository/ClubRepository.java
@@ -2,6 +2,8 @@ package com.example.smu_club.club.repository;
 
 import com.example.smu_club.domain.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
 }
