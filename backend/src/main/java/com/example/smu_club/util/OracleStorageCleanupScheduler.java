0aeba9e752afd191537203f1f65db5f970f71675
FluanceIFI <gksrnr66@gmail.com>
2025-12-15 22:20:59 +0900

    주석처리
    걍 오류 안뜨게 주석처리해둠

--- backend/src/main/java/com/example/smu_club/util/OracleStorageCleanupScheduler.java
+++ backend/src/main/java/com/example/smu_club/util/OracleStorageCleanupScheduler.java
@@ -18,34 +18,34 @@ public class OracleStorageCleanupScheduler {
     private final OciStorageService ociStorageService;
     private final AnswerRepository answerRepository;
 
-    @Scheduled(cron = "0 0 3 * * *") // 매일 새벽 3시에 실행
-    public void cleanUpOrphanFiles() {
-        log.info("==고아 파일 삭제 스케줄러 시작==");
-
-        //List<String> storedFileUrls = ociStorageService.getOldFileKeys();
-
-        if(storedFileUrls .isEmpty()) {
-            log.info("==삭제할 파일이 없습니다==");
-            return;
-        }
-
-        //List<String> dbFileUrls = answerRepository.findAllFileKeys();
-        //Set<String> validKeys = new HashSet<>(dbFileUrls);
-
-        int deleteCount = 0;
-        List<String> deleteKeyList = new ArrayList<>();
-        for(String key : storedFileUrls) {
-            if(!validKeys.contains(key)) {
-                deleteKeyList.add(key);
-                deleteCount++;
-            }
-        }
-
-        if(!deleteKeyList.isEmpty()) {
-            ociStorageService.deleteUrls(deleteKeyList);
-            log.info("==고아 파일 삭제 완료: {}개 파일 삭제==", deleteCount);
-        } else {
-            log.info("==삭제할 파일이 없습니다==");
-        }
-    }
+//    @Scheduled(cron = "0 0 3 * * *") // 매일 새벽 3시에 실행
+//    public void cleanUpOrphanFiles() {
+//        log.info("==고아 파일 삭제 스케줄러 시작==");
+//
+//        //List<String> storedFileUrls = ociStorageService.getOldFileKeys();
+//
+//        if(storedFileUrls .isEmpty()) {
+//            log.info("==삭제할 파일이 없습니다==");
+//            return;
+//        }
+//
+//        //List<String> dbFileUrls = answerRepository.findAllFileKeys();
+//        //Set<String> validKeys = new HashSet<>(dbFileUrls);
+//
+//        int deleteCount = 0;
+//        List<String> deleteKeyList = new ArrayList<>();
+//        for(String key : storedFileUrls) {
+//            if(!validKeys.contains(key)) {
+//                deleteKeyList.add(key);
+//                deleteCount++;
+//            }
+//        }
+//
+//        if(!deleteKeyList.isEmpty()) {
+//            ociStorageService.deleteUrls(deleteKeyList);
+//            log.info("==고아 파일 삭제 완료: {}개 파일 삭제==", deleteCount);
+//        } else {
+//            log.info("==삭제할 파일이 없습니다==");
+//        }
+//    }
 }
