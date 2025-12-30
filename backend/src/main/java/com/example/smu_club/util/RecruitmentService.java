1b12782e67dd247aa40b3c3d76aa8f0bfe5fe922
FluanceIFI <gksrnr66@gmail.com>
2025-12-30 17:46:45 +0900

    복구

--- backend/src/main/java/com/example/smu_club/util/RecruitmentService.java
+++ backend/src/main/java/com/example/smu_club/util/RecruitmentService.java
@@ -14,31 +14,31 @@ import java.util.List;
 @Service
 public class RecruitmentService {
 
-    @Transactional(readOnly = true)
-    public List<ClosureTarget> findEndedClubs(LocalDateTime now) {
-        String time = now.toString();
-
-
-    }
-
-    @Transactional
-    public void closeRecruitments(List<ClosureTarget> list) {
-    }
-
-    @RequiredArgsConstructor
-    @Getter
-    @Builder
-    public static class ClosureTarget {
-        private final long clubId;
-        private final RecruitingStatus previousStatus;
-
-        public static ClosureTarget from(Club club) {
-            return ClosureTarget.builder()
-                    .clubId(club.getId())
-                    .previousStatus(club.getRecruitingStatus())
-                    .build();
-        }
-
-
-    }
+//    @Transactional(readOnly = true)
+//    public List<ClosureTarget> findEndedClubs(LocalDateTime now) {
+//        String time = now.toString();
+//
+//
+//    }
+//
+//    @Transactional
+//    public void closeRecruitments(List<ClosureTarget> list) {
+//    }
+//
+//    @RequiredArgsConstructor
+//    @Getter
+//    @Builder
+//    public static class ClosureTarget {
+//        private final long clubId;
+//        private final RecruitingStatus previousStatus;
+//
+//        public static ClosureTarget from(Club club) {
+//            return ClosureTarget.builder()
+//                    .clubId(club.getId())
+//                    .previousStatus(club.getRecruitingStatus())
+//                    .build();
+//        }
+//
+//
+//    }
 }
