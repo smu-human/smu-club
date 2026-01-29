7b3aebb6f3e4c20c0b5957edaa08bd909578eadd
BonggusCha <aayy4321@naver.com>
2026-01-29 23:18:08 +0900

    Setting: CI 관련 설정

--- backend/src/test/java/com/example/smu_club/ClubRepositoryTest.java
+++ backend/src/test/java/com/example/smu_club/ClubRepositoryTest.java
@@ -8,11 +8,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import static org.awaitility.Awaitility.await;
-import java.util.concurrent.TimeUnit;
 import java.util.List;
 import java.util.Optional;
 
