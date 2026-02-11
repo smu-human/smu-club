04e7970b793dcaaa2035f30feab24a74673a29cc
LEEYOUNPYO <lypyin3@gmail.com>
2026-02-12 03:21:01 +0900

    feature/2.12modify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,10 +8,8 @@ export default defineConfig({
     host: "0.0.0.0", // Docker 외부 접속 허용
     port: 3000, // 포트 명시 (선택 사항)
     proxy: {
-      // '/api'로 시작하는 요청이 오면 백엔드로 전달
       "/api": {
-        // eslint-disable-next-line no-undef
-        target: process.env.VITE_API_URL || "http://localhost:8080", // Docker Compose 서비스 이름 사용 target: "http://backend:8080"
+        target: "http://localhost:8080",
         changeOrigin: true,
         secure: false,
       },
