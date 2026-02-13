f7e261841cbbfc82cedfeac699cecc0f87968045
LUVIZU <lypyin3@gmail.com>
2026-02-13 19:08:42 +0900

    feature/2.19modify (#177)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Docker 외부 접속 허용
-    port: 3000, // 포트 명시 (선택 사항)
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
