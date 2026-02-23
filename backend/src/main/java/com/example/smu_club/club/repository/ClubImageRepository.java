c38907bb94f3729b85d7483b1e3028eea6d7bda2
BonggusCha <aayy4321@naver.com>
2026-02-24 00:35:05 +0900

    Hotfix: 기존 스케줄러가 이미지 파일까지 삭제하던 버그 수정  (#184)
    * feature/2.19modify
    
    * HotFix: Deploy 과정에서 Nginx 최신 반영 안되는 상황 발견 및 수정
    
    * feature/2.13modify
    
    * feature/2.15modify
    
    * Update README.md
    
    * Update README.md
    
    * feature/2.21modify
    
    * HotFix: 현재 스케줄러가 이미지 파일까지 제거하는거 확인 및 이미지 파일키 검사하는 로직 추가 (#183)
    
    ---------
    
    Co-authored-by: LEEYOUNPYO <lypyin3@gmail.com>
    Co-authored-by: FluanceIFI <111571081+fluanceifi@users.noreply.github.com>

--- backend/src/main/java/com/example/smu_club/club/repository/ClubImageRepository.java
+++ backend/src/main/java/com/example/smu_club/club/repository/ClubImageRepository.java
@@ -2,6 +2,7 @@ package com.example.smu_club.club.repository;
 
 import com.example.smu_club.domain.ClubImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,10 @@ public interface ClubImageRepository extends JpaRepository<ClubImage, Long> {
 
 
     List<ClubImage> findAllByClubId(Long clubId);
+
+
+    @Query("SELECT c.imageFileKey FROM ClubImage c")
+    List<String> findAllFileKeys();
+
 }
+
